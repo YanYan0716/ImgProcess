7e68ddd7ef16cc08c07f5c24fb4a2a8727378f85
yanqian <yanqian0716@gmail.com>
2022-02-24 21:17:46 +0800

    .

--- 17ImageRestorationAndTheWienerFilter.py
+++ 17ImageRestorationAndTheWienerFilter.py
@@ -27,10 +27,10 @@ def motion_process(img, motion_angle=60):
 
 def make_blurred(img, eps=1e-3):
     PSF = motion_process(img)
-    b_gray, g_gray, r_gray = cv2.split(img.copy())
+    b, g, r = cv2.split(img.copy())
     result = []
-    for gray in [b_gray, g_gray, r_gray]:
-        gray_fft = np.fft.fft2(gray)
+    for c in [b, g, r]:
+        gray_fft = np.fft.fft2(c)
         psf_fft = np.fft.fft2(PSF) + eps
         blurred = np.fft.ifft2(gray_fft*psf_fft)
         blurred = np.abs(np.fft.fftshift(blurred))
@@ -38,20 +38,47 @@ def make_blurred(img, eps=1e-3):
     return result
 
 
-def wiener():
-    pass
+def wiener(img, eps=1e-3, K=0.01):
+    '''
+    reference:https://blog.csdn.net/wsp_1138886114/article/details/95024180
+    维纳滤波目的：复原图像和原图像的均方误差最小
+    :return:
+    '''
+    PSF = motion_process(img)
+    b, g, r = cv2.split(img.copy())
+    result = []
+    for c in [b, g, r]:
+        # 根据傅里叶变换的特性，空间域中的卷积相当于频率域中的乘积
+
+        # 将空间域的图片转换到频率域
+        input_fft = np.fft.fft2(c)
+        #将噪声转换到频率域
+        PSF_fft = np.fft.fft2(PSF)+eps
+
+        PSF_fft = np.conj(PSF_fft)/(np.abs(PSF_fft)**2+K)
+        res = np.fft.ifft2(input_fft*PSF_fft)
+        # 将结果转回空间域
+        res = np.abs(np.fft.fftshift(res))
+        result.append(res)
+    return result
 
 
-figure = plt.figure(num='ImageRestorationAndTheWienerFilter', figsize=(6, 6))
+figure = plt.figure(num='ImageRestorationAndTheWienerFilter', figsize=(6, 3))
 img = cv2.imread('./data/IMG21.jpg')
-plt.subplot(2, 2, 1)
+plt.subplot(1, 3, 1)
 plt.title('origin')
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
-PSF = make_blurred(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-PSF = np.abs(np.transpose(np.array(PSF, dtype=np.uint), [1, 2, 0]))
-print(PSF)
-plt.subplot(2, 2, 2)
+blurred_img = make_blurred(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+blurred_img = np.abs(np.transpose(np.array(blurred_img, dtype=np.uint), [1, 2, 0]))
+plt.subplot(1, 3, 2)
 plt.title('blur')
-plt.imshow(PSF)
+plt.imshow(blurred_img)
+
+
+restoration = wiener(np.float32(blurred_img))
+restoration = np.abs(np.transpose(np.array(restoration, dtype=np.uint), [1, 2, 0]))
+plt.subplot(1, 3, 3)
+plt.title('restoration')
+plt.imshow(restoration)
 plt.show()
