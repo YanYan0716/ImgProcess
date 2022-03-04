a5d29e7289c74e38911e2ae28f501662c0b63ed2
yanqian <yanqian0716@gmail.com>
2022-03-04 21:08:28 +0800

    .

--- 19Fan-beamReconstruction.py
+++ 19Fan-beamReconstruction.py
@@ -1,3 +1,8 @@
 '''
 reference:https://www.youtube.com/watch?v=ETlgxFcxelw&list=PLuh62Q4Sv7BUf60vkjePfcOQc8sHxmnDX&index=22
 '''
+
+
+'''https://blog.csdn.net/kouwang9779/article/details/116307803'''
+import numpy as np
+import math
