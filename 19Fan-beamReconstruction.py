61d481a56a42483f7c744f90b3ec9110213df978
yanqian <yanqian0716@gmail.com>
2022-03-04 20:54:01 +0800

    .

---
+++ 19Fan-beamReconstruction.py
@@ -0,0 +1,3 @@
+'''
+reference:https://www.youtube.com/watch?v=ETlgxFcxelw&list=PLuh62Q4Sv7BUf60vkjePfcOQc8sHxmnDX&index=22
+'''
---
+++ 20DitheringAndHalftoning.py
@@ -0,0 +1,3 @@
+'''
+reference:https://www.youtube.com/watch?v=UJtV3DdjCVY&list=PLuh62Q4Sv7BUf60vkjePfcOQc8sHxmnDX&index=23&t=2s
+'''
